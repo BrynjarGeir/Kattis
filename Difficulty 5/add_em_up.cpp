05a7c4d1dacc6a9b7e7bb45c10d04e166a54cddf
BrynjarGeir <brynjargeir1@gmail.com>
2022-03-02 12:15:26 +0000

    .

---
+++ Difficulty 5/add_em_up.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, s;
+    string number;
+    cin >> n >> s;
+    vector<int> cards;
+
+    for(int i = 0; i < n; i++) {
+        cin >> number;
+        
+        for()
+        
+    }
+}
