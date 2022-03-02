34c73d8d21c1f498de16cbe2076e5f63aa1ccfa2
BrynjarGeir <35775978+BrynjarGeir@users.noreply.github.com>
2022-03-02 10:35:20 +0000

    .

---
+++ Difficulty 8/3sideddice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d1d1, d1d2, d1d3, d2d1, d2d2, d2d3, d3d1, d3d2, d3d3, target1, target2, target3;
+    
+    cin >> d1d1 >> d1d2 >> d1d3 >> d2d1 >> d2d2 >> d2d3 >> d3d1 >> d3d2 >> d3d3 >> target1 >> target2 >> target3;
+    while(d1d1 != 0 && d1d2 != 0 && d1d3 != 0) {
+
+    }
+}
---
+++ Difficulty 8/sneakattack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    double x1,y1,x2,y2,x3,y3, angle, orientation;
+
+    int b = 1500;
+
+    while(b--) {
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+        angle = atan((y1-y2) / (x1-x2));
+
+        
+
+    }
+}
